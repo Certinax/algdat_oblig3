d147dd18ad0d63e31e70f648624537714bcf5c1a
Certinax <s319217@oslomet.no>
2018-10-25 20:56:06 +0200

    Oppgave 1 ok oppg 2 trenger updates, fungerer ikke

--- src/no/certinax/Main.java
+++ src/no/certinax/Main.java
@@ -6,10 +6,26 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-
+        /*
         System.out.println("Oblig 3");
+        {
+            ObligSBinTre<String> tre = new ObligSBinTre<>(Comparator.naturalOrder());
+            System.out.println(tre.antall());
+        }
 
-        ObligSBinTre<String> tre = new ObligSBinTre<>(Comparator.naturalOrder());
-        System.out.println(tre.antall());
+        {
+            Integer[] a = {4, 7, 2, 9, 5, 10, 8, 1, 3, 6};
+            ObligSBinTre<Integer> tre = new ObligSBinTre<>(Comparator.naturalOrder());
+            for (int verdi : a) tre.leggInn(verdi);
+            System.out.println(tre.antall());
+        }
+        */
+        {
+            ObligSBinTre<Integer> tre = new ObligSBinTre<>(Comparator.naturalOrder());
+            int[] a = {1,7,1,6,1,5,1,4,1,1,1,3};
+            for (int verdi : a) tre.leggInn(verdi);
+
+            System.out.println(tre.antall(1));
+        }
     }
 }
--- src/no/certinax/ObligSBinTre.java
+++ src/no/certinax/ObligSBinTre.java
@@ -46,7 +46,29 @@ public class ObligSBinTre<T> implements Beholder<T>
     @Override
     public boolean leggInn(T verdi)
     {
-        throw new UnsupportedOperationException("Ikke kodet ennå!");
+        Objects.requireNonNull(verdi, "Ulovlig med nullverdier!");
+
+        Node<T> p = rot, q = null;              // p starter i roten
+        int cmp = 0;                            // hjelpevariabel
+
+        while (p != null)                       // fortsetter til p er ute av treet
+        {
+            q = p;
+            cmp = comp.compare(verdi,p.verdi);  // bruker komparatoren
+            p = cmp < 0 ? p.venstre : p.høyre;  // flytter p
+
+        }
+
+        // p er nå null, dvs.ute av treet, q er den siste vi passerte
+
+        p = new Node<>(verdi,q);               // oppretter en ny node
+
+        if (q == null) rot = p;                 // p blir rotnode
+        else if (cmp < 0) q.venstre = p;        // venstre barn til q
+        else q.høyre = p;                       // høyre barn til q
+
+        antall++;                               // én verdi mer i treet
+        return true;                            // vellykket innlegging
     }
 
     @Override
@@ -86,7 +108,20 @@ public class ObligSBinTre<T> implements Beholder<T>
 
     public int antall(T verdi)
     {
-        throw new UnsupportedOperationException("Ikke kodet ennå!");
+        if(verdi == null) return 0;
+        int count = 0;
+        Node<T> p = rot;
+
+
+        while(p != null) {
+            int cmp = comp.compare(verdi, p.verdi);
+            if(cmp == 0) count++;
+            if(cmp < 0) p = p.venstre;
+            else if(cmp > 0) p = p.høyre;
+            else break;
+        }
+        
+        return count;
     }
 
     @Override
