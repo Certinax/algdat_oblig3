361787d8ea7383aa727f7efd0bf232fa2d4b58b7
Certinax <s319217@oslomet.no>
2018-10-25 18:19:59 +0200

    Oppgave 0, OK. Skriver ut 0

--- src/no/certinax/Main.java
+++ src/no/certinax/Main.java
@@ -1,10 +1,15 @@
 package no.certinax;
 
+import java.util.Comparator;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
 
         System.out.println("Oblig 3");
+
+        ObligSBinTre<String> tre = new ObligSBinTre<>(Comparator.naturalOrder());
+        System.out.println(tre.antall());
     }
 }
